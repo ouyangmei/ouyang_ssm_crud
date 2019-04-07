3b2b5b306a2c5b2d55ef7916e0deec1adcf97a0c
ouyangmei <1142378169@qq.com>
2019-04-07 22:21:26 +0800

    第一天呀

--- src/main/Java/com/ouyang/crud/test/MapperTest.java
+++ src/main/Java/com/ouyang/crud/test/MapperTest.java
@@ -1,6 +1,5 @@
 package com.ouyang.crud.test;
 
-import com.ouyang.crud.bean.Department;
 import com.ouyang.crud.bean.Employee;
 import com.ouyang.crud.dao.DepartmentMapper;
 import com.ouyang.crud.dao.EmployeeMapper;
@@ -29,8 +28,8 @@ public class MapperTest {
     @Autowired
     DepartmentMapper departmentMapper;
 
-   @Autowired
-   EmployeeMapper employeeMapper;
+    @Autowired
+    EmployeeMapper employeeMapper;
 
     @Autowired
     SqlSession sqlSession;
@@ -40,22 +39,22 @@ public class MapperTest {
      */
     @Test
     public void testCRUD(){
-      //  1.创建SpringIOC容器
-      /*  ApplicationContext ioc= new ClassPathXmlApplicationContext("applicationContext.xml");
+        /*//1.创建SpringIOC容器
+        ApplicationContext ioc= new ClassPathXmlApplicationContext("applicationContext.xml");
         //2.从容器中获取Mapper
         ioc.getBean(DepartmentMapper.class);*/
-        System.out.println(departmentMapper);
+        //System.out.println(departmentMapper);
         //1.插入几个部门
-       /* departmentMapper.insertSelective(new Department(null,"开发部"));
-        departmentMapper.insertSelective(new Department(null,"测试部"));*/
+        //departmentMapper.insertSelective(new Department(null,"开发部"));
+        //departmentMapper.insertSelective(new Department(null,"测试部"));
 
         //2.生成员工数据，测试员工插入
-      //  employeeMapper.insertSelective(new Employee(null,"Jerry","M","Jerry@ecut.com",1));
+        //employeeMapper.insertSelective(new Employee(null,"Jerry","M","Jerry@ecut.com",1));
         //employeeMapper.insertSelective(new Employee(null,"TOM","N","TOM@ecut.com",1));
 
         //3.批量插入多个员工；使用可执行批量操作的sqlSession
         EmployeeMapper mapper=sqlSession.getMapper(EmployeeMapper.class);
-        for(int i=0;i<10;i++){
+        for (int i = 0; i < 100; i++) {
             String uid=UUID.randomUUID().toString().substring(0,5)+i;
             mapper.insertSelective(new Employee(null,uid,"M",uid+"@ecut.com",1));
         }
